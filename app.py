2bce35bc4383d36ad5c17a8df6938c2d4073654a
akhil1993-prog <akhilkfscb@gmail.com>
2026-06-27 13:58:33 +0530

    Enable dynamic port configuration for Flask app
    Added environment variable support for port configuration.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # ഇവിടെ നമ്മൾ index.html ഫയലിനെ കാണിക്കാൻ ഫ്ലാസ്കിനോട് ആവശ്യപ്പെടുന്നു
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
